198cfe84467c821c11d7a0d9652faf71ef5b011e
Spenser Saling <spenser0saling@gmail.com>
2020-09-19 09:15:30 -0700

    Add solitaire link to footer

--- src/data/links.js
+++ src/data/links.js
@@ -69,7 +69,7 @@ const links = {
 
 
 // NO HARD CODED LINKS HERE.
-//THIS IS WHERE LINKS CAN BE GROUPED/ORDERED FOR USE IN THE SITE
+// THIS IS WHERE LINKS CAN BE GROUPED/ORDERED FOR USE IN THE SITE
 const linksOrdered = {
 //-----------------     FOOTER LINKS     ---------------
   byCategory: [
@@ -86,6 +86,7 @@ const linksOrdered = {
       links: [
         {name: 'Feel With Me', url: links.feelWithMe.home},
         {name: 'Sailing To Mars', url: '/'},
+        {name: 'Solitaire', url: links.solitaire.home}
       ]
     },
     {
